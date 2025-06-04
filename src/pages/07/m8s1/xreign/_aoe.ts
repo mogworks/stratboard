919d9a538de27164bc6ee731183f6e9afa3d18ec
Super Jump <superjump.void@gmail.com>
2025-06-04 19:48:05 +0800

    xreign 4/n

--- src/pages/07/m8s1/xreign/_aoe.ts
+++ src/pages/07/m8s1/xreign/_aoe.ts
@@ -32,3 +32,18 @@ export function createRectAoE(app: Application, W: number, H: number, params: { 
     return c
   })
 }
+
+export function createFanAoE(app: Application, R: number, params: { r: number; a: number; A: number; rotation: number }[]) {
+  return splitContainer(() => {
+    const c = new Container()
+    params.forEach((p) => {
+      const radius = p.r * YmToPx
+      const radian = (p.a * Math.PI) / 180
+      const fan = AoE.createFan(R, p.A).toSprite(app)
+      fan.position.set(radius * Math.cos(radian), radius * Math.sin(radian))
+      fan.rotation = p.rotation * Math.PI / 180
+      c.addChild(fan)
+    })
+    return c
+  })
+}
