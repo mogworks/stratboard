4c6d92365775785d721bc13713fbd1906d26ed00
Super Jump <superjump.void@gmail.com>
2025-06-09 20:13:41 +0800

    m7s 标点数据

--- src/pages/07/m7s/_waymark.ts
+++ src/pages/07/m7s/_waymark.ts
@@ -4,3 +4,12 @@ export const waymarkDataDB: WaymarkData = {
   D: { X: 3.4, Z: 3.4 },
   B: { X: -3.4, Z: -3.4 },
 }
+
+export const waymarkDataAC1234: WaymarkData = {
+  A: { X: 0, Z: -10 },
+  C: { X: 0, Z: 10 },
+  One: { X: -10, Z: -10 },
+  Two: { X: 10, Z: -10 },
+  Three: { X: 10, Z: 10 },
+  Four: { X: -10, Z: 10 },
+}
